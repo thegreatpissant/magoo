35aa094345d3b1c439b37ffea2d9c0892b6bebcd
JAF <openjaf@gmail.com>
2020-05-31 00:25:15 -0400

    Update the default ISS-Sim bot script. The craft almost levels out now. Added forward movement

--- magoo.py
+++ magoo.py
@@ -36,6 +36,7 @@ target_z = 0.0
 prev_time = time.perf_counter()
 prev_y = self.browser_window.get_value("positionY")
 prev_z = self.browser_window.get_value("positionZ")
+prev_x = self.browser_window.get_value("positionX")
 
 while self.run_bot:
     if orientation_tick % orientation_tick_slice == 0: 
@@ -62,27 +63,35 @@ while self.run_bot:
     current_time = time.perf_counter()
     current_y = self.browser_window.get_value("positionY")
     current_z = self.browser_window.get_value("positionZ")
+    current_x = self.browser_window.get_value("positionX")
     delta_time = current_time - prev_time
     delta_y = current_y - prev_y
     delta_z = current_z - prev_z
+    delta_x = current_x - prev_x
     current_y_speed = delta_y / delta_time
     current_z_speed = delta_z / delta_time
+    current_x_speed = delta_x / delta_time
     predicted_y = current_y + (current_y_speed * tick_slice)
     predicted_z = current_z + (current_z_speed * tick_slice)
+    predicted_x = current_x + (current_x_speed * tick_slice)
     if translation_tick % translation_tick_slice == 0:
         if predicted_y > target_y:
             self.browser_window.execute_command("translateLeft")
         if predicted_y < target_y:
             self.browser_window.execute_command("translateRight")
-
         if predicted_z > target_z:
             self.browser_window.execute_command("translateDown")
         if predicted_z < target_z:
             self.browser_window.execute_command("translateUp")
+        if predicted_x > target_x:      
+            self.browser_window.execute_command("translateBackward")
+        if predicted_x < target_x:
+            self.browser_window.execute_command("translateForward")
     #  Time delay
     prev_time = current_time
     prev_y = current_y
     prev_z = current_z
+    prev_x = current_x
     time.sleep(tick_slice -  (time.perf_counter() - current_time))
     translation_tick += 1
     orientation_tick += 1
